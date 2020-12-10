057d88c7b868ff5ade94b661e1b1dda2e36245c7
Karan singh <kssinghkaran13@gmail.com>
2020-12-10 17:05:06 +0530

    Final Keeper app

---
+++ build/precache-manifest.6796227d3fd92ec4282298f2c6738b75.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf8d8e1b8b38baf03e6e725b65e316e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7db87d1ae09bd69ccd5b",
+    "url": "/static/js/2.80ac774c.chunk.js"
+  },
+  {
+    "revision": "870c85ed54a10a420f80",
+    "url": "/static/js/main.d360625b.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
